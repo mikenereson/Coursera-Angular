b682d73fbb67480621565ba1763c08c2e14411dc
MikeNereson <mnereson@gmail.com>
2018-03-23 12:30:55 -0400

    comments, removed unneeded getUnpurchased function

--- Week2/Assignments/Assignment2/app.js
+++ Week2/Assignments/Assignment2/app.js
@@ -5,6 +5,10 @@
         .controller("ShoppingListShowController", ShoppingListShowController)
         .service("ShoppingListService", ["$rootScope", ShoppingListService]);
 
+
+    /**
+     * ShoppingListAddController is responsible for the Add Item form
+     */
     ShoppingListAddController.$inject = ["$scope", "ShoppingListService"];
 
     function ShoppingListAddController($scope, ShoppingListService) {
@@ -18,6 +22,7 @@
         addCtrl.labelState = "";
         addCtrl.quantityState = "";
 
+        /* validate teh user's input, returns true when valid */
         addCtrl.isValid = function () {
             addCtrl.clearState();
             /* label must have a value */
@@ -28,6 +33,7 @@
             return !addCtrl.labelState && !addCtrl.quantityState;
         };
 
+        /* when input is valid, add a new item */
         addCtrl.addItem = function () {
             if (!addCtrl.isValid()) {
                 return;
@@ -37,28 +43,40 @@
             addCtrl.clear();
         };
 
+        /* clear the new item form */
         addCtrl.clear = function () {
             addCtrl.label = "";
             addCtrl.quantity = "";
         };
+
+        /* reset the validation state */
         addCtrl.clearState = function () {
             addCtrl.labelState = "";
             addCtrl.quantityState = "";
         }
     }
 
+    /**
+     * ShoppingListShowController is responsible for the two shopping lists: purchased and unpurchased
+     */
     ShoppingListShowController.$inject = ["$scope", "ShoppingListService"];
 
     function ShoppingListShowController($scope, ShoppingListService) {
         var showCtrl = this;
 
+        // subscribe to the itemsChanged event that the ShoppingListService triggers
+        // whenever the items change.
         $scope.$on("itemsChanged", refresh);
+
+        // init the lists
         refresh();
 
+        // remove an item from the shopping lists
         showCtrl.removeItem = function (index) {
             ShoppingListService.removeItem(index);
         };
 
+        // change the purchased state of an item
         showCtrl.togglePurchased = function (index, purchased) {
             ShoppingListService.togglePurchased(index, purchased);
         };
@@ -67,9 +85,9 @@
             window.print();
         };
 
+        // sync this controller's lists with the service
         function refresh() {
             showCtrl.items = ShoppingListService.getAllItems();
-            showCtrl.unpurchasedItems = ShoppingListService.getAllUnpurchasedItems();
             showCtrl.purchasedItems = ShoppingListService.getAllPurchasedItems();
         }
     }
@@ -80,33 +98,31 @@
 
         // List of shopping items
         var items = [];
-        //items.push({label: "default purchased item", quantity: 1, purchased: true});
 
+        // Add a new, unpurchased item to the shopping list
         service.addItem = function (label, quantity) {
             items.push({label: label, quantity: quantity, purchased: false});
             $rootScope.$broadcast('itemsChanged');
         };
 
+        // Add an item.
         service.removeItem = function (index) {
             items.splice(index, 1);
             $rootScope.$broadcast('itemsChanged');
         };
 
+        // change one item's purchased state
         service.togglePurchased = function (index, purchased) {
             items[index].purchased = purchased;
             $rootScope.$broadcast('itemsChanged');
         };
 
+        // get all items on the shopping list
         service.getAllItems = function () {
             return items;
         };
 
-        service.getAllUnpurchasedItems = function () {
-            return items.filter(function (item) {
-                return !item.purchased;
-            });
-        };
-
+        // get only the purchased items on the shopping list
         service.getAllPurchasedItems = function () {
             return items.filter(function (item) {
                 return item.purchased;
